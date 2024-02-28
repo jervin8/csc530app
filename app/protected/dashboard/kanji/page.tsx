b12e7e300a32ad187c4de4b614b4b22d2b697326
John Ervin <jervin8@murraystate.edu>
2024-02-28 09:11:41 -0600

    added authentication checks to the protected pages

---
+++ app/protected/dashboard/kanji/page.tsx
@@ -0,0 +1,16 @@
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+
+export default async function Kanji() {
+    const supabase = createClient();
+  
+    //redirects to login page if not an authenticated/logged in user
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+  
+    if (!user) {
+      return redirect("/login");
+    }
+}
