e4462734b89ab0c77133de6f771acf3e8b81a3d7
mpayne30 <Mpayne30@murraystate.edu>
2024-03-05 21:41:40 -0600

    minor changes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,21 +5,23 @@ import Footer from "@/components/Footer";
 
 export default function About() {
   return (
-    <div className="h-screen w-full bg-gray-100 dark:bg-slate-700 text-black dark:text-white">
+    <main className="h-full w-full bg-gray-100 dark:bg-slate-700 text-black dark:text-white">
         <Navbar />
-        <div>
-            <Image src={AboutBackground} alt="About Us" width={500} height={300} layout="responsive"/>
-        </div>
+        <div className="h-screen">
+          <div>
+              <Image src={AboutBackground} alt="About Us" width={500} height={300}/>
+          </div>
 
-        <div className="relative">
-            <p className="text-lg">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-              suscipit ligula et metus tincidunt, ut interdum nisi congue.
-              Pellentesque habitant morbi tristique senectus et netus et malesuada
-              fames ac turpis egestas.
-            </p>
+          <div className="relative">
+              <p className="text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+                suscipit ligula et metus tincidunt, ut interdum nisi congue.
+                Pellentesque habitant morbi tristique senectus et netus et malesuada
+                fames ac turpis egestas.
+              </p>
+          </div>
         </div>
         <Footer/>
-    </div>
+    </main>
   );
 }
