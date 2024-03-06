9e848e38803e63acaf4841c10a479db9a7dfd47c
mpayne30 <Mpayne30@murraystate.edu>
2024-03-05 19:40:57 -0600

    Landing page and minor navbar fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
 import Navbar from "@/components/Navbar";
+import PrimarySection from "@/components/LandingPageComps/PrimarySection";
+import SectionTitle from "@/components/LandingPageComps/SectionTitle";
+import Testimonials from "@/components/LandingPageComps/Testimonials";
 import Footer from "@/components/Footer";
 
 export default function Landing() {
-    return (
-    <main className="h-screen w-full">
-      <Navbar />
 
-      <div className="bg-white pagecontainer h-full pt-20">
-        <div className="container mx-auto text-black">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et egestas quis ipsum suspendisse ultrices gravida dictum. Netus et malesuada fames ac turpis egestas sed tempus urna. Vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra. Mattis nunc sed blandit libero. Potenti nullam ac tortor vitae purus faucibus. Est ultricies integer quis auctor elit sed vulputate. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Praesent elementum facilisis leo vel fringilla. Diam sit amet nisl suscipit adipiscing bibendum est ultricies. Faucibus a pellentesque sit amet porttitor eget dolor morbi non. Proin nibh nisl condimentum id venenatis a condimentum vitae. Arcu cursus euismod quis viverra nibh cras pulvinar mattis nunc. Venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin. Sed faucibus turpis in eu mi bibendum. Volutpat lacus laoreet non curabitur gravida. Fames ac turpis egestas sed tempus urna et pharetra. Sem integer vitae justo eget magna fermentum. Tortor at risus viverra adipiscing at in tellus integer.</p>
-          <p>Massa tempor nec feugiat nisl pretium fusce. Convallis convallis tellus id interdum velit laoreet id donec. Suspendisse in est ante in. Id aliquet risus feugiat in ante metus dictum at tempor. Justo laoreet sit amet cursus sit amet dictum sit amet. Non sodales neque sodales ut etiam sit amet. Bibendum arcu vitae elementum curabitur. Faucibus pulvinar elementum integer enim. Neque ornare aenean euismod elementum nisi quis. Maecenas volutpat blandit aliquam etiam erat velit.</p>
-          <p>Turpis massa tincidunt dui ut ornare lectus sit. Amet consectetur adipiscing elit duis. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo. Lectus sit amet est placerat in egestas. Sit amet tellus cras adipiscing enim. Venenatis lectus magna fringilla urna porttitor. Est ante in nibh mauris cursus mattis molestie. Fermentum et sollicitudin ac orci phasellus. Risus sed vulputate odio ut enim blandit volutpat maecenas volutpat. A erat nam at lectus urna duis convallis. Netus et malesuada fames ac turpis. Adipiscing at in tellus integer. Convallis a cras semper auctor.</p>
-        </div>
-      </div>
+  return (
+    <main className="h-full w-full bg-white dark:bg-slate-900">
+      <Navbar />
+      <PrimarySection />
+      <SectionTitle pretitle="Space Repetition Benefits" title=" Why should you learn through space repitition">
+        Space repetition is a learning technique that involves reviewing information at increasing intervals over time. 
+        It offers several benefits for language learning:
+        Improved Retention, Efficiency, Time Management, Long-Term Recall, and Adaptability
+      </SectionTitle>
+      {/*
+      <SectionTitle pretitle="Watch a video" title="Learn how to fullfil your needs">
+        This section is to highlight a promo or demo video of your product.
+        Analysts says a landing page with video has 3% more conversion rate. So,
+        don&apos;t forget to add one. Just like this.
+      </SectionTitle>
+      <Video />
+      */}
+      <SectionTitle pretitle="Testimonials" title="Here's what our customers said">
+        These are a.i. generated
+      </SectionTitle>
+      <Testimonials />
       <Footer />
     </main>
-    )
+  );
 }
