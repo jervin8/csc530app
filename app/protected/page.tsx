8338cf31fb42968746e298cc70077307f7606307
mpayne30 <Mpayne30@murraystate.edu>
2024-04-03 00:41:06 -0500

    Styles

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -8,6 +8,7 @@ import VocabReviewButton from "@/components/ProtectedPageComps/VocabReviewButton
 import VocabLessonButton from "@/components/ProtectedPageComps/VocabLessonButton";
 import KanjiLessonButton from "@/components/ProtectedPageComps/KanjiLessonButton";
 import WordCountGraph from "@/components/ProtectedPageComps/WordCountGraph";
+import ReviewForecast from "@/components/ProtectedPageComps/ReviewForecast";
 
 
 export default async function Dashboard() {
@@ -29,18 +30,48 @@ return (
   <main className="h-full w-full bg-gray-200 dark:bg-slate-700 text-black dark:text-white">
     <Navbar/>
     <div className="pt-20">
-        <div className="h-screen container mx-auto">
-            <h1>Vocab</h1>
-            <VocabLessonButton />
-            <VocabReviewButton />
-            <h1>Kanji</h1>
-            <KanjiLessonButton />
-            <KanjiReviewButton />
-            <div className=" bg-white text-black w-1/2 rounded-lg p-5">
-            <WordCountGraph />
-            </div>
+      <div className="w-3/4 h-full container m-auto grid grid-cols-6 grid-rows-4 gap-10 place-items-center">
+        <div className="w-full h-full rounded-lg bg-indigo-600 col-span-2 p-5">
+          <h1>Vocab</h1>
+          <VocabLessonButton />
+          <VocabReviewButton />
         </div>
-       
+        
+        <div className="w-full h-full rounded-lg bg-indigo-600 col-span-2 p-5">
+          <h1>Kanji</h1>
+          <KanjiLessonButton />
+          <KanjiReviewButton />
+        </div>
+
+        <div className="w-full h-full rounded-lg bg-indigo-600 col-span-2 row-span-3">
+          {/*Review forcast type thing here. take up two rows in the thrid colmn slot*/}
+          <ReviewForecast />
+        </div>
+
+        <div className=" bg-white text-black w-full h-full rounded-lg p-5 col-span-4 row-span-2">
+          <WordCountGraph />
+        </div>
+
+        <div className="w-full h-full m-auto grid grid-cols-5 col-span-6 place-items-center">
+          <div className="w-3/4 h-3/4 rounded-lg bg-indigo-600 p-5">
+            Rookie
+          </div>
+          <div className="w-3/4 h-3/4 rounded-lg bg-indigo-600 p-5">
+            Amateur
+          </div>
+          <div className="w-3/4 h-3/4 rounded-lg bg-indigo-600 p-5">
+            Expert
+          </div>
+          <div className="w-3/4 h-3/4 rounded-lg bg-indigo-600 p-5">
+            Master
+          </div >
+          <div className="w-3/4 h-3/4 rounded-lg bg-indigo-600 p-5">
+            Gigachad
+          </div>
+        </div>
+      </div>
+
+      
     <Footer />
     </div>
   </main>
---
+++ components/ProtectedPageComps/ReviewForecast.tsx
@@ -0,0 +1,28 @@
+// components/ReviewForecast.js
+
+import React from 'react';
+
+function ReviewForecast() {
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const currentDate = new Date();
+  const currentDayIndex = currentDate.getDay(); // 0 for Sunday, 1 for Monday, etc.
+  const currentDay = daysOfWeek[currentDayIndex];
+  
+  // Generate a list of the remaining days of the week
+  const remainingDays = daysOfWeek.slice(currentDayIndex + 1).concat(daysOfWeek.slice(0, currentDayIndex));
+
+  return (
+    <div className="m-auto p-5">
+      <h1>Review Forecast</h1>
+      <p>Today:</p>      
+      <ul>
+        {/*Gonna turn these into components eventually that recieves review related stuff from DB*/}
+        {remainingDays.map((day, index) => (
+          <li key={index}>{day}:</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ReviewForecast;
