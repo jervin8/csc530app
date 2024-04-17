fc40409b2284bcd935c318d8ca910f898dbb1730
mpayne30 <Mpayne30@murraystate.edu>
2024-04-16 21:05:48 -0500

    Exit button style fix

--- components/ProtectedPageComps/ExitButton.tsx
+++ components/ProtectedPageComps/ExitButton.tsx
@@ -9,6 +9,6 @@ export default function ExitButton() {
     }
 
     return(
-        <button onClick={buttonClicked} type="button" className="absolute top-0 left-0 text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">Exit</button>
+        <button onClick={buttonClicked} type="button" className="absolute top-0 left-0 text-indigo-600 hover:text-white border border-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-indigo-500 dark:text-indigo-500 dark:hover:text-white dark:hover:bg-indigo-600 dark:focus:ring-indigo-900">Exit</button>
     )
 }
