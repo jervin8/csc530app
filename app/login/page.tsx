28b1707e81f23de8b52accd97f7f71ca99940b68
bkethallard <ballard@murraystate.edu>
2024-04-16 19:28:58 -0500

    navbar

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
 import { SubmitButton } from "./submit-button";
+import Navbar from "@/components/Navbar";
 
 export default function Login({
   searchParams,
@@ -52,10 +53,11 @@ export default function Login({
   };
 
   return (
-
+    <main className="h-full w-full bg-gray-200 dark:bg-slate-700 text-black dark:text-white">
+      <Navbar/>
     <div className="h-screen flex items-center justify-center w-full gap-2 bg-gray-200 dark:bg-slate-700 text-black dark:text-white">
       
-
+ 
 
         <form className="animate-in flex-1 flex flex-col max-w-md justify-center gap-2 text-foreground">
           <label className="text-md text-black" htmlFor="email">
@@ -99,5 +101,6 @@ export default function Login({
         </form>
       
     </div>
+    </main>
   );
 }
