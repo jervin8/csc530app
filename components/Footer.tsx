24ff2236b3cd65f880c2e1150b8384281a3d3bdb
mpayne30 <Mpayne30@murraystate.edu>
2024-04-18 14:32:50 -0500

    Logo Updates

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,13 @@
+import Image from 'next/image';
+import LogoImage from "@/app/public/img/logo.png";
+
 export default function Footer() {
     return (
         <footer className="bg-white shadow dark:bg-gray-900 mt-auto">
             <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="SRSLLAPP Logo" />
+                    <Image src={LogoImage} className=""  width={50} alt="Logo"/>
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SRSLL APP</span>
                     </a>
                     <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,15 +17,9 @@ if(user){
     return (
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-<<<<<<< Updated upstream
-            <a href="/protected" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SR.SLLAPP</span>
-=======
                 <a href="/protected" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image src={LogoImage} className=""  width={50} alt="Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SRSLLAPP</span>
->>>>>>> Stashed changes
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                     <span className="sr-only">Open main menu</span>
@@ -45,7 +39,7 @@ if(user){
                         <a href="/pricing" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 dark:text-white md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
                         </li>
                         <li>
-                        <a href="/protected/Profile" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 dark:text-white md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</a>
+                        <a href="/protected/profile" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-indigo-700 md:p-0 dark:text-white md:dark:hover:text-indigo-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Profile</a>
                         </li>
                     </ul>
                 </div>
@@ -59,8 +53,8 @@ else{
     return(
         <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <Image src={LogoImage} className=""  width={50} alt="Logo"/>
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SRSLLAPP</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
